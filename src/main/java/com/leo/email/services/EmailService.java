b258d766b91e26d855b2b10787d9490c430027d0
Leonardo250 <leomoulin2002@gmail.com>
2025-12-08 22:13:52 -0300

    metodo para o envio de email e controle de exceção feitos

--- src/main/java/com/leo/email/services/EmailService.java
+++ src/main/java/com/leo/email/services/EmailService.java
@@ -1,9 +1,15 @@
 package com.leo.email.services;
 
+import com.leo.email.enums.StatusEmail;
 import com.leo.email.models.EmailModel;
 import com.leo.email.repsitories.EmailRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
@@ -11,7 +17,24 @@ public class EmailService {
     @Autowired
     EmailRepository emailRepository;
 
-    public void sendEmail(EmailModel emailModel) {
+    @Autowired
+    private JavaMailSender emailSender;
 
+    public EmailModel sendEmail(EmailModel emailModel) {
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        }catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        }finally {
+            return emailRepository.save(emailModel);
+        }
     }
 }
